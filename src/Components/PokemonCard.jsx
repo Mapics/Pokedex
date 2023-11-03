0ce2c41a77d48e06264d670298f7e26c47493022
Mapics <nadjarmatt@gmail.com>
2023-11-03 02:33:12 +0100

    rien

--- src/Components/PokemonCard.jsx
+++ src/Components/PokemonCard.jsx
@@ -6,9 +6,9 @@ export default function PokemonCard({pokemon}) {
     const [loading , setLoading] = useState(true);
 
     useEffect(() => {
-    fetch(`${pokemon.url}`, { 
-      method: "GET",
-    }).then((res) => res.json()) // convert data to json
+      fetch(`${pokemon.url}`, { 
+        method: "GET",
+      }).then((res) => res.json()) // convert data to json
       .then((data) => {
         setPokemonInfo(data);
         setLoading(false);
@@ -17,16 +17,23 @@ export default function PokemonCard({pokemon}) {
         console.log("error", err)
       })
     }, [])
-
+    
+    const addToTeam = () => {
+      const team = JSON.parse(localStorage.getItem("team")) || []
+      team.push(pokemonInfo)
+      localStorage.setItem("team", JSON.stringify(team))
+    }
 
   return (
     <div className="pokemon-card">
+
       <div className="pokemon-image">
         <img
           src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonInfo.id}.png`}
           alt={pokemonInfo.name}
         />
       </div>
+
       <div className="pokemon-details">
         <p>Nom: {pokemon.name}</p>
         <p>Numéro: {pokemonInfo.id}</p>
@@ -38,7 +45,7 @@ export default function PokemonCard({pokemon}) {
             "Chargement en cours..."
         )}</p>
 
-        <button>Ajouter au Pokedex</button>
+        <button class="raise" onClick={addToTeam}>Ajouter au Pokedex</button>
       </div>
     </div>
   )
